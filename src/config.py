b455a2f1b7d084a58c38d8e81592c77374de032a
Gargi <gargikekre95@gmail.com>
2021-05-23 13:10:02 +0530

    strategy1

---
+++ src/config.py
@@ -0,0 +1,11 @@
+item1='BANKNIFTY'
+market_key1 ="e7f57c31e7a4dd7a2d3610"
+market_secret1="Yryw010@@Q"
+order_key1="ad56ca1e5385d29696f579"
+order_secret1="Xyjh028$83"
+
+item2='NIFTY'
+market_key2="ba719abe2b60934786a595"
+market_secret2="Xnfd504@0Q"
+order_key2="6009acbafd3d3a4df97871"
+order_secret2="Cccm635#ht"
