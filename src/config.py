0d8bebba9ec497f804c62d12ef5fa191368608d0
arunima811 <guddan@W106JNC2X2.blr.apac.dell.com>
2021-05-25 19:57:47 +0530

    FIX : configurable ymls

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 item=item1
 # 'BANKNIFTY'
-market_key="market_key1"
+market_key=market_key1
 # "e7f57c31e7a4dd7a2d3610"
-market_secret="market_secret1"
+market_secret=market_secret1
 # "Yryw010@@Q"
-order_key="order_key1"
+order_key=order_key1
 # "ad56ca1e5385d29696f579"
-order_secret="order_secret1"
+order_secret=order_secret1
 # "Xyjh028$83"
