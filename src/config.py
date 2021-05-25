dceebf8dae5a75de0645ac3e9229e4cc2f3dd470
arunima811 <guddan@W106JNC2X2.blr.apac.dell.com>
2021-05-25 18:58:27 +0530

    Fix: configurable params

--- src/config.py
+++ src/config.py
@@ -1,5 +1,10 @@
-item1='BANKNIFTY'
-market_key1 ="e7f57c31e7a4dd7a2d3610"
-market_secret1="Yryw010@@Q"
-order_key1="ad56ca1e5385d29696f579"
-order_secret1="Xyjh028$83"
+item=item1
+# 'BANKNIFTY'
+market_key="market_key1"
+# "e7f57c31e7a4dd7a2d3610"
+market_secret="market_secret1"
+# "Yryw010@@Q"
+order_key="order_key1"
+# "ad56ca1e5385d29696f579"
+order_secret="order_secret1"
+# "Xyjh028$83"
